a386ead720fe124b7f6e46b58bc8e75b12a57ef8
ShubhamLohra <officialshubhamlohra@gmail.com>
2023-11-22 14:26:26 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public static void main (String args[]){
    System.out.println("This is java program");
       System.out.println("output from java program : ");
    System.out.println(new Date());
-
+    System.out.println("Hello");
 
 }
 
